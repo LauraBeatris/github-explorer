d6c2ebabe1f6e5c71bce0da3144267bfaf9b21ac
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-07 13:52:32 -0300

    refactor(ui): extract logic of adding a repository to hook

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+import { Repository } from "shared/types";
+
 export const api = axios.create({
   baseURL: "https://api.github.com",
 });
 
+export const getRepository = async (repositoryName: string) => {
+  const { data } = await api.get<Repository>(`repos/${repositoryName}`);
+
+  return data;
+};

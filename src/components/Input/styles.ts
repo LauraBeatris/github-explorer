1b6a4a84c200279aa1d38568abb289cabbf959dd
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-07 13:51:15 -0300

    refactor(ui): implement input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,22 @@
+import styled, { css } from "styled-components";
+
+import { InputProps } from "./types";
+
+export const StyledInput = styled.input<InputProps>`
+  flex: 1;
+  height: 70px;
+  padding: 0 24px;
+  border: 0;
+  border-radius: 5px 0 0 5px;
+  border: 1px solid ${({ theme }) => theme.colors.background};
+
+  ${({ hasError, theme }) => hasError
+    && css`
+      border: 2px solid ${theme.colors.error};
+      border-right: 0;
+    `};
+
+  @media screen and (max-width: 600px) {
+    flex: unset;
+  }
+`;

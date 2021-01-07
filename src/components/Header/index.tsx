8e3c283785ad8d8e313205039613051727ac251a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-07 12:01:50 -0300

    refactor(ui): modify header to be composable

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { FiChevronLeft } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import React, { PropsWithChildren } from "react";
 
 import logo from "assets/logo.svg";
 
 import { Container } from "./styles";
-import { HeaderProps } from "./types";
 
-export function Header({ backToDashboard }: HeaderProps) {
+export function Header({ children }: PropsWithChildren<unknown>) {
   return (
     <Container>
       <img
@@ -17,12 +14,7 @@ export function Header({ backToDashboard }: HeaderProps) {
         title="GitHub Explorer | Explore amazing repositories"
       />
 
-      {backToDashboard && (
-        <Link to="/">
-          <FiChevronLeft size={16} />
-          Voltar
-        </Link>
-      )}
+      {children}
     </Container>
   );
 }

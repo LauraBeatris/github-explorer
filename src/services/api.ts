d93ec485f3aec20ecadebc5caf903efd7a029eda
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-12 21:09:08 -0300

    chore: add new api fetch functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,25 @@
 import axios from "axios";
 
-import { Repository } from "shared/githubAPI";
+import { Issue, Repository, PullRequest } from "shared/githubAPI";
 
 export const api = axios.create({
   baseURL: "https://api.github.com",
 });
 
-export const getRepository = async (repositoryName: string) => {
-  const { data } = await api.get<Repository>(`repos/${repositoryName}`);
+export async function getRepository(repositoryFullName: string) {
+  const { data } = await api.get<Repository>(`repos/${repositoryFullName}`);
 
   return data;
-};
+}
+
+export async function getIssues(repositoryFullName: string) {
+  const { data } = await api.get<Issue[]>(`repos/${repositoryFullName}/issues`);
+
+  return data.filter((item) => !item.pull_request);
+}
+
+export async function getPullRequests(repositoryFullName: string) {
+  const { data } = await api.get<PullRequest[]>(`repos/${repositoryFullName}/pulls`);
+
+  return data;
+}
